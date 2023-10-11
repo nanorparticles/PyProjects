8de42593c86e7838f14aa9aaa3852407be30aaf8
t-anano <ananor@aggies.ncat.edu>
2023-10-11 18:59:08 -0400

    Transposition Ciphers

---
+++ civil_war.py
@@ -0,0 +1,6 @@
+# Decoding American Civil War Cyphers 
+# Cryptography is the science of secure communication through the use of codes and ciphers 
+# General Anson Stager of the North developed a route tranportation cypher that became one of the most sucessful
+# military ciphers of all time 
+
+# The objective is to write an algorithm for the South that can decrypt Northern Civil War Cyphers
