4c2545f07ee711f16a3b3489c18d12836ba555cd
t-anano <ananor@aggies.ncat.edu>
2023-10-28 12:31:34 -0400

    Define fitness function and cull population

--- super_rats.py
+++ super_rats.py
@@ -21,3 +21,24 @@ if NUM_RATS % 2 != 0:
     NUM_RATS += 1
 
 
+# This is the programs shopping representative, picks out the rats. 
+def populate(num_rats, min_wt, max_wt, mode_wt):
+    "Initialize a population with a Triangular distribution of weights"
+    return [int(random.triangular(min_wt, max_wt, mode_wt)) for i in range(num_rats)]
+
+def fitness(population, goal):
+    "Measure population fitness based on attribute mean vs target mean"
+    ave = statistics.mean(population)
+    return ave / goal 
+
+def select(population, to_retain):
+    """Cull a population to retain only a specified number of members"""
+    sorted_population = sorted(population)
+    to_retain_by_sex = to_retain //2
+    members_per_sex = len(sorted_population)//2
+    females = sorted_population[:members_per_sex]
+    males = sorted_population[members_per_sex:]
+    selected_females = females[-to_retain_by_sex:]
+    selected_males = males[-to_retain_by_sex:]
+    return selected_males, selected_females
+
