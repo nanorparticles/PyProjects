c684292f12971ce2caa1d199d65dd840fd5900fb
t-anano <ananor@aggies.ncat.edu>
2023-10-29 17:00:57 -0400

    Define the main function

--- super_rats.py
+++ super_rats.py
@@ -14,7 +14,7 @@ MUTATE_MIN = 0.5
 MUTATE_MAX = 1.2 
 LITTER_SIZE = 8
 LITTERS_PER_YEAR = 10 
-GENERATIONAL_LIMIT = 500
+GENERATION_LIMIT = 500
 
 # ensure even number of rats for breeding pairs 
 if NUM_RATS % 2 != 0: 
@@ -61,8 +61,33 @@ def breed(males, females, litter_size):
 def mutate(children, mutate_odds, mutate_min, mutate_max):
     """Randomly alter rat weights using input odds and fractional changes"""
     for index, rat in enumerate(children): 
-        if mutate_odds >= random.random(): 
+        if mutate_odds >= random.random(): # If the mutate odds are greater than or equal to the randomly generated number // the rat weight at the index is mutated
             children[index] = round(rat* random.uniform(mutate_min, mutate_max))
     return children 
 
 
+
+def main(): 
+    """Initialize population, select, breed, mutate, display results"""
+    generations = 0
+    parents = populate(NUM_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT, INITIAL_MODE_WT)
+    print("Initial Population Weights = {} ".format(parents))
+    popl_fitness = fitness(parents, GOAL)
+    print("initial population fitness = {}".format(popl_fitness))
+    print("number to retain = {}".format(NUM_RATS)) 
+
+    ave_wt = []
+
+    while popl_fitness < 1 and generations < GENERATION_LIMIT: 
+        selected_males, selected_females = select(parents, NUM_RATS)
+        children = breed(selected_males, selected_females, LITTER_SIZE)
+        children = mutate(children, MUTATE_ODDS, MUTATE_MIN, MUTATE_MAX)
+        parents = selected_males + selected_females + children
+        popl_fitness = fitness(parents, GOAL)
+        print("Generation {} fitness = {:.4f}".format(generations, popl_fitness))
+        ave_wt.append(int(statistics.mean(parents)))
+        generations+=1
+    print("average weight per generation = {}".format(ave_wt))
+    print("\nnumber of generations = {}".format(generations))
+    print("number of years = {}".format(int(generations/LITTERS_PER_YEAR)))
+      
